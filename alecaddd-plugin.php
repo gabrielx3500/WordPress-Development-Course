2a5ea9f22a5eba359db8c6a7c55ce09dd528797a
Gabriel <gabrielx3500@gmail.com>
2019-04-15 17:01:08 -0430

    Desarrollamos el script de enqueue para agregar .CSS y .JS

--- alecaddd-plugin.php
+++ alecaddd-plugin.php
@@ -1,5 +1,5 @@
 <?php 
-/**
+/** Video N° 6
   * @package AlecadddPlugin
   */
 /*
@@ -23,6 +23,10 @@ class AlecadddPlugin
 	function __construct(){
       add_action('init',array($this,'custom_post_type'));
 	}
+
+	function register(){
+	  add_action('admin_enqueue_scripts',array($this,'enqueue'));	
+	}
 	function activate(){
       $this->custom_post_type();
       //flush_rewrite_rules();
@@ -39,12 +43,18 @@ class AlecadddPlugin
 	function custom_post_type(){
       register_post_type('book',['public'=>true,'label'=>'Books']);
 	}
+
+	function enqueue(){
+      wp_enqueue_style('mypluginstyle',plugins_url('/assets/mystyle.css',__FILE__));
+      wp_enqueue_script('mypluginscript',plugins_url('/assets/myscript.js',__FILE__));
+	}
 	
 	
 }
 
 if(class_exists('AlecadddPlugin')){
 	$alecadddPlugin = new alecadddPlugin('Alecaddd Plugin. initialized!');
+	$alecadddPlugin->register();
 }
 //activation
 register_activation_hook(__FILE__,array($alecadddPlugin,'activate'));
